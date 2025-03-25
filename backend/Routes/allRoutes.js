9dfe71403e6c32bf3f69eafac73762ad87c04c0b
TomYenialem <tom21g2008@gmail.com>
2025-03-25 10:02:46 +0300

    add patinets all routes

--- backend/Routes/allRoutes.js
+++ backend/Routes/allRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const route=express.Router();
 const login =require('./login.routes')
 const employee=require('./employee.routes')
+const patinets=require('./patient.routes')
 
 route.use(login)
 route.use( employee)
+route.use(patinets)
 
 module.exports = route;
